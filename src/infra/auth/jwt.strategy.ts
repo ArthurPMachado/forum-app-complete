3a2be688609aea9bc40ba9cf2ebd8aa19e044f7c
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-02 15:49:15 -0300

    refactor: add env module and services to remove infer on env variables

--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -1,17 +1,16 @@
 import { ExtractJwt, Strategy } from 'passport-jwt'
-import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
 import { Injectable } from '@nestjs/common'
-import { Env } from '../env'
 import {
   UserPayloadSchema,
   userPayloadSchema,
 } from '../http/schemas/token-schema'
+import { EnvService } from '../env/env.service'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService<Env, true>) {
-    const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+  constructor(env: EnvService) {
+    const publicKey = env.get('JWT_PUBLIC_KEY')
 
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
