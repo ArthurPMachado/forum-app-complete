8b54ead8f3af48dee57fbd78d945246075caa630
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-04 15:51:41 -0300

    test: add e2e factories for comments and answer

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -2,6 +2,9 @@ import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { IAnswerCommentProps } from '@/domain/forum/enterprise/entities/interfaces/ICommentProps'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper'
 
 export function makeAnswerComment(
   override: Partial<IAnswerCommentProps> = {},
@@ -18,4 +21,21 @@ export function makeAnswerComment(
   )
 
   return answerComment
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerComment(
+    data: Partial<IAnswerCommentProps> = {},
+  ): Promise<AnswerComment> {
+    const questionComment = makeAnswerComment(data)
+
+    await this.prisma.comment.create({
+      data: PrismaAnswerCommentMapper.toPrisma(questionComment),
+    })
+
+    return questionComment
+  }
 }
--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -2,6 +2,9 @@ import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { IQuestionCommentProps } from '@/domain/forum/enterprise/entities/interfaces/ICommentProps'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaQuestionCommentMapper } from '@/infra/database/prisma/mappers/prisma-question-comment-mapper'
 
 export function makeQuestionComment(
   override: Partial<IQuestionCommentProps> = {},
@@ -18,4 +21,21 @@ export function makeQuestionComment(
   )
 
   return questionComment
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionComment(
+    data: Partial<IQuestionCommentProps> = {},
+  ): Promise<QuestionComment> {
+    const questionComment = makeQuestionComment(data)
+
+    await this.prisma.comment.create({
+      data: PrismaQuestionCommentMapper.toPrisma(questionComment),
+    })
+
+    return questionComment
+  }
 }
