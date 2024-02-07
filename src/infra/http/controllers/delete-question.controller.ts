665597ceee27cb096b78a1ab64cadf72aad7b751
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-07 13:18:59 -0300

    feat: add delete question controller

---
+++ src/infra/http/controllers/delete-question.controller.ts
@@ -0,0 +1,33 @@
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  HttpCode,
+  Param,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayloadSchema } from '../schemas/token-schema'
+import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
+
+@Controller('/questions/:id')
+export class DeleteQuestionController {
+  constructor(private deleteQuestion: DeleteQuestionUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayloadSchema,
+    @Param('id') questionId: string,
+  ) {
+    const { sub: userId } = user
+
+    const result = await this.deleteQuestion.execute({
+      questionId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
