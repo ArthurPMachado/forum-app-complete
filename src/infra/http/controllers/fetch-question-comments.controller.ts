3d3dd48b36f0c88348ef0aec38bf92d3aeb23acf
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-09 15:50:09 -0300

    feat: fetch question comments controller

---
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -0,0 +1,42 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import {
+  PageQueryParamSchema,
+  pageQueryParamSchema,
+} from '../schemas/page-query-param-schema'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
+import { CommentPresenter } from '../presenters/comment-presenter'
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions/:questionId/comments')
+export class FetchQuestionCommentsController {
+  constructor(private fetchQuestionComments: FetchQuestionCommentsUseCase) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamSchema,
+    @Param('questionId') questionId: string,
+  ) {
+    const result = await this.fetchQuestionComments.execute({
+      page,
+      questionId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    const { questionComments } = result.value
+
+    return {
+      comments: questionComments.map(CommentPresenter.toHTTP),
+    }
+  }
+}
