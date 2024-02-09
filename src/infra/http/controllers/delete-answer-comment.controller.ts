05e30ced122a18ecf4e92e267344e0465e81eedc
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-09 12:16:48 -0300

    feat: delete  answer comment controller

---
+++ src/infra/http/controllers/delete-answer-comment.controller.ts
@@ -0,0 +1,33 @@
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  HttpCode,
+  Param,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayloadSchema } from '../schemas/token-schema'
+import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment'
+
+@Controller('/answers/comments/:id')
+export class DeleteAnswerCommentController {
+  constructor(private deleteAnswerComment: DeleteAnswerCommentUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayloadSchema,
+    @Param('id') answerCommentId: string,
+  ) {
+    const { sub: userId } = user
+
+    const result = await this.deleteAnswerComment.execute({
+      answerCommentId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
