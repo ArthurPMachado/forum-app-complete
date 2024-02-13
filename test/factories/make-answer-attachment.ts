60d2c2404251f54c35754b42f2398f65c765771e
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-13 18:55:30 -0300

    test: answer question and edit answer attachments on end to end tests

--- test/factories/make-answer-attachment.ts
+++ test/factories/make-answer-attachment.ts
@@ -1,6 +1,8 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { IAnswerAttachmentProps } from '@/domain/forum/enterprise/entities/interfaces/IAnswerAttachmentProps'
 import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 export function makeAnswerAttachment(
   override: Partial<IAnswerAttachmentProps> = {},
@@ -16,4 +18,26 @@ export function makeAnswerAttachment(
   )
 
   return answer
+}
+
+@Injectable()
+export class AnswerAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerAttachment(
+    data: Partial<IAnswerAttachmentProps> = {},
+  ): Promise<AnswerAttachment> {
+    const answerAttachment = makeAnswerAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: answerAttachment.attachmentId.toString(),
+      },
+      data: {
+        answerId: answerAttachment.answerId.toString(),
+      },
+    })
+
+    return answerAttachment
+  }
 }
