2c9bbaef431fe66dc0ac26f37a7ebe731bf6920a
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-13 16:16:46 -0300

    test: create and edit attachments when creating or editing a question end to end

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -4,13 +4,13 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
-import { PrismaQuestionAttachmentsRepository } from './prisma-question-attachments-repository'
+import { IQuestionAttachmentsRepository } from '@/domain/forum/application/repositories/interfaces/question-attachments-repository'
 
 @Injectable()
 export class PrismaQuestionsRepository implements IQuestionsRepository {
   constructor(
     private prisma: PrismaService,
-    private questionAttachmentsRepository: PrismaQuestionAttachmentsRepository,
+    private questionAttachmentsRepository: IQuestionAttachmentsRepository,
   ) {}
 
   async create(question: Question): Promise<void> {
